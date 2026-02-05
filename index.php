eac66fb4fd990c2939a6b6db4342ccfff8d5a158
BabyAdy <moiseadrianionut7@gmail.com>
2026-02-05 05:57:36 +0100

    add navbar

--- index.php
+++ index.php
@@ -18,13 +18,45 @@ if (isset($_SESSION['discord_id'])) {
 </head>
 <body>
 
-<?php if (!$user): ?>
-    <a href="login.php">Login cu Discord</a>
-<?php else: ?>
-    Salut, <?= $user['username'] ?> |
-    <?= getAdminRank($user['admin_level']) ?>
-    <a href="logout.php">Logout</a>
-<?php endif; ?>
+<div class="navbar">
+    <div class="nav-left">
+        <div class="logo">LIBERTY.MP</div>
+
+        <div class="nav-menu">
+            <a href="#">Home</a>
+            <a href="#">Staff</a>
+            <a href="#">Forums</a>
+            <a href="#">Factions</a>
+            <a href="#">Clans</a>
+            <a href="#">Shop</a>
+            <a href="#">Posts</a>
+            <a href="#">More</a>
+            <a href="#">Updates</a>
+            <a href="#">Staff Applications</a>
+        </div>
+    </div>
+
+    <div class="nav-right">
+        <div class="search-box">
+            <input type="text" placeholder="Search...">
+        </div>
+
+        <?php if ($user): ?>
+            <div class="user-box">
+                <img src="https://cdn.discordapp.com/avatars/<?= $user['discord_id'] ?>/<?= $user['avatar'] ?>.png">
+                <span class="user-name"><?= $user['username'] ?></span>
+
+                <div class="dropdown">
+                    <a href="#">Go to profile</a>
+                    <a href="logout.php">Logout</a>
+                </div>
+            </div>
+        <?php else: ?>
+            <a href="login.php" class="login-btn">Login</a>
+        <?php endif; ?>
+    </div>
+</div>
+
 
 <hr>
 
